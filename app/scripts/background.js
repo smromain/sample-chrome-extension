305881f0127bcf32c057fb97df440ffdf7e16616
Steve Romain <smromain@gmail.com>
2014-11-24 18:48:40 -0500

    stripped down extension to bare minimum

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,8 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  chrome.tabs.executeScript({
+    code: 'document.body.innerHTML = "<h1>i am totally working right now no need to look over my shoulder please leave me alone</H1>"; document.body.appendChild(document.createElement("img")).src="http://lorempixel.com/800/600/cats/"',
+  });
 });
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
