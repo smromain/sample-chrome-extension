afb15d13b6b6bc889e6e7b82d13dfec804602523
Steve Romain <smromain@gmail.com>
2014-11-25 10:06:39 -0500

    finished specs and fixed notification message

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,8 +2,10 @@
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
+  
   chrome.tabs.executeScript({
-    code: 'document.body.innerHTML = "<h1>i am totally working right now no need to look over my shoulder please leave me alone</H1>"; document.body.appendChild(document.createElement("img")).src="http://lorempixel.com/800/600/cats/"',
+    code: 'document.body.innerHTML = ""; document.body.appendChild(document.createElement("img")).src="http://i.imgur.com/B7cXd.png"',
   });
-  chrome.notifications.create('myNotification', {type: 'basic', title: 'Hello World!', message: 'Nothing to see here.', iconUrl: 'images/cat.jpg', priority: 2}, function () {console.log('notification was created')})
+
+  chrome.notifications.create('myNotification', {type: 'basic', title: 'Hello World!', message: 'Nothing to see here.', iconUrl: 'images/cat.jpg', priority: 2}, function () {console.log('basic text notification was created')});
 });
