cdd7608c2a0b9630f01923c1e0f0a02baee73555
Steve Romain <smromain@gmail.com>
2014-11-25 09:27:21 -0500

    added notification message

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,4 +5,5 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
     code: 'document.body.innerHTML = "<h1>i am totally working right now no need to look over my shoulder please leave me alone</H1>"; document.body.appendChild(document.createElement("img")).src="http://lorempixel.com/800/600/cats/"',
   });
+  chrome.notifications.create('myNotification', {type: 'basic', title: 'Hello World!', message: 'Nothing to see here.', iconUrl: 'images/cat.jpg', priority: 2}, function () {console.log('notification was created')})
 });
